f36a23647de5bcaadbc21ac49d91db27bc2eb046
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2022-10-26 19:33:08 +0200

    add: mapstruck dependency

--- src/main/java/org/hbrs/project/wram/control/user/UserService.java
+++ src/main/java/org/hbrs/project/wram/control/user/UserService.java
@@ -2,7 +2,9 @@ package org.hbrs.project.wram.control.user;
 
 import org.hbrs.project.wram.model.user.User;
 import org.hbrs.project.wram.model.user.UserDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     public UserDTO toDTO(User user) {
         UserDTO dto = UserDTO.builder().username(user.getUsername()).id(user.getId()).email(user.getEmail()).passwort(user.getPassword()).build();
