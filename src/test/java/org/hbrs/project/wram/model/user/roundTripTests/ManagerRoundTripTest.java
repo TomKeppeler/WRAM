038bd9cd2438a55a7b490770f3e8a7c90ffaae06
SophiaNow <92376130+SophiaNow@users.noreply.github.com>
2022-11-11 12:07:12 +0100

    remove unused import statement

--- src/test/java/org/hbrs/project/wram/model/user/roundTripTests/ManagerRoundTripTest.java
+++ src/test/java/org/hbrs/project/wram/model/user/roundTripTests/ManagerRoundTripTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.UUID;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
