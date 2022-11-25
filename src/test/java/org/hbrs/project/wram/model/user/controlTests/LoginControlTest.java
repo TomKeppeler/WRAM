8a957716da8ca005e102d3a54e0f0370536398c6
Salah <salah.khalosi@smail.inf.h-brs.de>
2022-11-25 11:12:14 +0100

    Code Smells beseitigen

--- src/test/java/org/hbrs/project/wram/model/user/controlTests/LoginControlTest.java
+++ src/test/java/org/hbrs/project/wram/model/user/controlTests/LoginControlTest.java
@@ -3,9 +3,6 @@ package org.hbrs.project.wram.model.user.controlTests;
 import org.hbrs.project.wram.control.LoginControl;
 import org.hbrs.project.wram.control.user.UserService;
 import org.hbrs.project.wram.model.user.User;
-import org.hbrs.project.wram.util.Encryption;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,19 +20,11 @@ class LoginControlTest {
 
     User user = new User();
 
-    @BeforeEach
-    void setUp(){
-
-    }
 
     @Test
     void testLoginAuthentication() throws Exception {
         assertFalse(loginControl.authenticateUser("maxMuster", "Test12345"));
     }
 
-    @AfterEach
-    void deleteFromDB(){
-
-    }
 
 }
