80427d240a7599795c1f7c9750fc3a658320f4db
Salah <salah.khalosi@smail.inf.h-brs.de>
2022-12-08 11:00:00 +0100

    EntwicklerAntraegeView

--- src/main/java/org/hbrs/project/wram/model/entwickler/EntwicklerRepository.java
+++ src/main/java/org/hbrs/project/wram/model/entwickler/EntwicklerRepository.java
@@ -26,6 +26,6 @@ public interface EntwicklerRepository extends JpaRepository<Entwickler, UUID> {
     public List<Entwickler> findAllByKundenprojektIsNull();
 
 
-    @Query("SELECT kundenprojektId from Entwickler entwickler where entwickler.id =: entwicklerID")
-    public List<Kundenprojekt> findAllKundenprojektIDtByEntwickler(UUID entwicklerID);
+    /*@Query("SELECT kundenprojektId from Entwickler entwickler where entwickler.id =: entwicklerID")
+    public List<Kundenprojekt> findAllKundenprojektIDtByEntwickler(UUID entwicklerID);*/
 }
